6d3e08fd829f2ca516155b3d8d15620ce09d502a
Loparev Alexander <dedomorozoff@gmail.com>
2026-05-18 02:08:49 +0700

    Add [ai] prefix to responses

--- internal/cli/render.go
+++ internal/cli/render.go
@@ -9,19 +9,20 @@ import (
 )
 
 func renderResponse(w io.Writer, resp prompt.Response, _ policy.Decision) {
+	fmt.Fprintf(w, "%s[ai]%s ", cyan, reset)
 	switch resp.Intent {
 	case prompt.IntentRunCommand:
 		if resp.Explanation != "" {
-			fmt.Fprintf(w, "  %s\n", resp.Explanation)
+			fmt.Fprintf(w, "%s%s%s\n", cyan, resp.Explanation, reset)
 		}
-		fmt.Fprintf(w, "  $ %s\n", resp.Command)
+		fmt.Fprintf(w, "%s$%s %s\n", gray, reset, resp.Command)
 	case prompt.IntentExplain:
-		fmt.Fprintln(w, resp.Explanation)
+		fmt.Fprintf(w, "%s%s%s\n", cyan, resp.Explanation, reset)
 	case prompt.IntentAskClarification:
 		if resp.Question != "" {
-			fmt.Fprintf(w, "  %s\n", resp.Question)
+			fmt.Fprintf(w, "%s%s%s\n", cyan, resp.Question, reset)
 		} else if resp.Explanation != "" {
-			fmt.Fprintf(w, "  %s\n", resp.Explanation)
+			fmt.Fprintf(w, "%s%s%s\n", cyan, resp.Explanation, reset)
 		}
 	}
 }
